4822a3da039b9b5a53ee6b7194946eec26c7a897
kennardc-CMU-HSL <kennardc@andrew.cmu.edu>
2026-02-07 15:46:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const videos = [
   {
-    src: 'https://drive.google.com/uc?export=download&id=1kRREF2U_eIeD8rSoC5wMK4s3SRg0YvyJ',
+    src: 'https://drive.google.com/file/d/1kRREF2U_eIeD8rSoC5wMK4s3SRg0YvyJ/preview',
     title: 'Video 1',
     description: 'This is the first video uploaded to Google Drive.'
   },
   {
-    src: 'https://drive.google.com/uc?export=download&id=1AV_FCwICdDQ85cOuDH48biwr1-31AQD8',
+    src: 'https://drive.google.com/file/d/1AV_FCwICdDQ85cOuDH48biwr1-31AQD8/preview',
     title: 'Video 2',
     description: 'This is the second video uploaded to Google Drive.'
   },
   {
-    src: 'https://drive.google.com/uc?export=download&id=1Nz3no2W_97WlTZzz1M_gxOD2twTxfCTq',
+    src: 'https://drive.google.com/file/d/1Nz3no2W_97WlTZzz1M_gxOD2twTxfCTq/preview',
     title: 'Video 3',
     description: 'This is the third video uploaded to Google Drive.'
   }
@@ -22,10 +22,14 @@ videos.forEach(video => {
   const card = document.createElement('div');
   card.className = 'video-card';
   card.innerHTML = `
-    <video controls>
-      <source src="${video.src}" type="video/mp4">
-      Your browser does not support HTML5 video.
-    </video>
+    <iframe 
+      src="${video.src}" 
+      width="100%" 
+      height="360" 
+      allow="autoplay" 
+      allowfullscreen 
+      frameborder="0">
+    </iframe>
     <h3>${video.title}</h3>
     <p>${video.description}</p>
   `;
